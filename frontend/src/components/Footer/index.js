539d6b3440ed681f46df2c861cadea340106dde6
Danny Shoun <danielshoun+servicesgithub@pm.me>
2021-04-27 01:44:56 -0400

    Formatting fixes.

--- frontend/src/components/Footer/index.js
+++ frontend/src/components/Footer/index.js
@@ -6,7 +6,7 @@ export default function Footer() {
     return (
         <div id='footer' className='footer'>
             <div className='footerDiv'></div>
-                <AudioPlayer/>
+            <AudioPlayer/>
             <div className='footerDiv'>
                 <i className="fab fa-github footerIcon" onClick={() => window.open('https://github.com/danielshoun/cast-cloud', '_blank')}/>
                 <i className="fab fa-linkedin footerIcon" onClick={() => window.open('https://www.linkedin.com/in/daniel-shoun/', '_blank')}/>
