bdfd22d6b048c6e1b66733f2d7dce2a3bf70c162
Danny Shoun <danielshoun+servicesgithub@pm.me>
2021-04-27 03:52:23 -0400

    Added progress bar, timestamps, and (non-functional) sound icon to footer player.

--- frontend/src/components/Footer/AudioPlayer.js
+++ frontend/src/components/Footer/AudioPlayer.js
@@ -2,19 +2,51 @@ import React, {useEffect, useRef, useState} from "react";
 import {useDispatch, useSelector} from "react-redux";
 import {togglePlaying} from "../../store/audio";
 import './AudioPlayer.css';
+import ProgressBar from "./ProgressBar";
 
 export default function AudioPlayer() {
     const audioState = useSelector(state => state.audio);
     const dispatch = useDispatch();
     const audioRef = useRef(null);
+    const[duration, setDuration] = useState(null);
+    const [curTime, setCurTime] = useState(null);
+    const [percentListened, setPercentListened] = useState(0);
 
     useEffect(() => {
-        audioRef.current.pause();
-        audioRef.current.load();
-        audioRef.current.addEventListener('canplay', () => {
+        function getTime(time) {
+            let hours = Math.floor(time / 3600);
+            time = time - hours * 3600;
+            let minutes = Math.floor(time / 60);
+            let seconds = Math.floor(time - minutes * 60);
+            let timeString = `${hours < 10 ? '0' + hours.toString(10) : hours}:` +
+                `${minutes < 10 ? '0' + minutes.toString(10) : minutes}:` +
+                `${seconds < 10 ? '0' + seconds.toString(10) : seconds}`;
+            setCurTime(timeString);
+            return timeString;
+        }
+
+        function readyPlayerState() {
+            setPercentListened(0);
+            setCurTime(getTime(audioRef.current.currentTime));
+            setDuration(getTime(audioRef.current.duration));
             audioRef.current.play();
             dispatch(togglePlaying(true));
-        })
+            audioRef.current.removeEventListener('canplay', readyPlayerState);
+        }
+
+        function updateTime(e) {
+            setPercentListened(e.target.currentTime / e.target.duration * 100)
+            setCurTime(getTime(e.target.currentTime));
+        }
+
+        audioRef.current.pause();
+        audioRef.current.load();
+        audioRef.current.addEventListener('canplay', readyPlayerState);
+        audioRef.current.addEventListener('timeupdate', updateTime);
+
+        return () => {
+            audioRef.current.removeEventListener('timeupdate', updateTime);
+        }
     }, [audioState.currentTrack?.url, dispatch])
 
     function playAudio() {
@@ -27,6 +59,10 @@ export default function AudioPlayer() {
         }
     }
 
+
+
+
+
     return (
         <div className='audioPlayerContainer'>
             <audio controls ref={audioRef}>
@@ -35,6 +71,8 @@ export default function AudioPlayer() {
             {audioState.playing ? <i className='fas fa-pause-circle controlButton' onClick={playAudio}/>
                 : <i className='fas fa-play-circle controlButton' onClick={playAudio}/>
             }
+            <ProgressBar duration={duration} curTime={curTime} percentListened={percentListened} audioRef={audioRef}/>
+            <i className="fas fa-volume-up volumeButton"/>
         </div>
 
     )
---
+++ frontend/src/components/Footer/ProgressBar.js
@@ -0,0 +1,42 @@
+import React, {useRef} from "react";
+
+export default function ProgressBar({duration, curTime, percentListened, audioRef}) {
+    const barRef = useRef(null);
+
+    function getClickedTime(e) {
+        const barStart = barRef.current.getBoundingClientRect().left + window.scrollX;
+        const barWidth = barRef.current.offsetWidth;
+        const clickPos = e.pageX - barStart;
+        const timePerPixel = audioRef.current.duration / barWidth;
+        return timePerPixel * clickPos;
+    }
+
+    function handleTimeDrag(e) {
+        audioRef.current.currentTime = getClickedTime(e);
+
+        function moveUpdate(e) {
+            audioRef.current.currentTime = getClickedTime(e);
+        }
+
+        document.addEventListener('mousemove', moveUpdate);
+        document.addEventListener('mouseup', () => {
+            document.removeEventListener('mousemove', moveUpdate);
+        })
+    }
+
+    return (
+        <div className='bar'>
+            <div
+                className='barProgress'
+                ref={barRef}
+                onMouseDown={handleTimeDrag}
+                style={{background: `linear-gradient(to right, #f35b04 ${percentListened}%, #1e1e24 0`}}>
+                    <span
+                        className='progressKnob'
+                        style={{left: `${percentListened - 2}%`}}
+                    />
+            </div>
+            <span className='barTime'>{curTime || '00:00:00'} | {duration || '00:00:00'}</span>
+        </div>
+    )
+}
