79ef809fa592c41c440739ea87e4ce1fc47100e7
Danny Shoun <danielshoun+servicesgithub@pm.me>
2021-04-29 12:21:44 -0400

    Created POST route for adding new comments.

--- backend/routes/api/episodes.js
+++ backend/routes/api/episodes.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const asyncHandler = require('express-async-handler')
+const {requireAuth} = require("../../utils/auth");
 const { Comment, User } = require('../../db/models');
 
 const router = express.Router();
@@ -9,5 +10,18 @@ router.get('/:episodeId/comments', asyncHandler(async (req, res) => {
     const comments = await Comment.findAll({where: {episodeId}, include: User})
     return res.json(comments);
 }))
+
+router.post('/:episodeId/comments', requireAuth, asyncHandler(async (req, res) => {
+    const episodeId = req.params.episodeId;
+    const userId = req.user.id;
+    const newComment = await Comment.create({
+        userId,
+        episodeId,
+        timestamp: req.body.timestamp,
+        text: req.body.text
+    })
+
+    return res.json(newComment);
+}));
 
 module.exports = router;
