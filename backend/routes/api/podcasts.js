4d7ff67d99feb2432726af3af422b4364faabd29
Danny Shoun <danielshoun+servicesgithub@pm.me>
2021-04-29 19:32:33 -0400

    Created backend route for getting a podcast's reviews. Added fetch for reviews in ReviewList component.

--- backend/routes/api/podcasts.js
+++ backend/routes/api/podcasts.js
@@ -2,7 +2,7 @@ const express = require('express');
 const asyncHandler = require('express-async-handler');
 const fetch = require("node-fetch");
 const Parser = require('rss-parser');
-const { Podcast, Episode } = require('../../db/models');
+const { Podcast, Episode, Review } = require('../../db/models');
 
 const parser = new Parser();
 
@@ -77,5 +77,12 @@ router.get('/:id/episodes', asyncHandler(async (req, res) => {
 
     return res.json(episodes);
 }))
+
+router.get('/:podcastId/reviews', asyncHandler(async (req, res) => {
+    const podcastId = req.params.podcastId;
+    const reviews = await Review.findAll({where: {podcastId}, order: [['createdAt', 'DESC']]});
+    return res.json(reviews);
+}))
+
 
 module.exports = router;
--- frontend/src/components/PodcastDetails/ReviewList.js
+++ frontend/src/components/PodcastDetails/ReviewList.js
@@ -1,7 +1,25 @@
-export default function ReviewList() {
+import {useEffect, useState} from "react";
+
+export default function ReviewList({podcastData}) {
+    const [reviewList, setReviewList] = useState([]);
+
+    useEffect(() => {
+        async function fetchData() {
+            const res = await fetch(`/api/podcasts/${podcastData.id}/reviews`);
+            const data = await res.json();
+            console.log(data);
+            setReviewList(data);
+        }
+        fetchData().then();
+    }, [podcastData])
+
     return (
         <>
-            Reviews
+            {reviewList.map(review => {
+                return (
+                    <div key={review.id}>review.text</div>
+                )
+            })}
         </>
     )
 }
