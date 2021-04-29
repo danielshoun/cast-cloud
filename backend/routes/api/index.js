17369181a1378473007bc031161728cec24e1fa2
Danny Shoun <danielshoun+servicesgithub@pm.me>
2021-04-29 17:45:44 -0400

    Added router file for comments API.

--- backend/routes/api/index.js
+++ backend/routes/api/index.js
@@ -3,11 +3,13 @@ const sessionRouter = require('./session');
 const usersRouter = require('./users');
 const podcastsRouter = require('./podcasts');
 const episodeRouter = require('./episodes');
+const commentRouter = require('./comments');
 
 const router = express.Router();
 router.use('/session', sessionRouter);
 router.use('/users', usersRouter);
 router.use('/podcasts', podcastsRouter);
-router.use('/episodes', episodeRouter)
+router.use('/episodes', episodeRouter);
+router.use('/comments', commentRouter);
 
 module.exports = router;
