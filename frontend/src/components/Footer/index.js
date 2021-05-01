01efb49f860e0ecc275deab1ce2aae40e1e051a2
Danny Shoun <danielshoun+servicesgithub@pm.me>
2021-05-01 01:45:14 -0400

    Added About info at bottom of page.

--- frontend/src/components/Footer/index.js
+++ frontend/src/components/Footer/index.js
@@ -2,11 +2,22 @@ import './Footer.css';
 import AudioPlayer from "./AudioPlayer";
 import AudioInfo from "./AudioInfo";
 import AudioPopups from "./AudioPopups";
+import {Link} from "react-router-dom";
 
 export default function Footer() {
 
     return (
         <>
+            <div className='aboutInfo'>
+                <div>
+                    Created by Danny Shoun.
+                    <Link to='https://github.com/danielshoun/cast-cloud'><i className="fab fa-github aboutIcon"/></Link>
+                    <Link to='https://www.linkedin.com/in/daniel-shoun/'><i className="fab fa-linkedin aboutIcon"/></Link>
+                </div>
+                <div>
+                </div>
+                <div><a href='mailto:danielshoun@protonmail.com' className='emailLink'>danielshoun@protonmail.com</a></div>
+            </div>
             <div className='push'/>
             <div id='footer' className='footer'>
                 <div className='footerDiv'>
